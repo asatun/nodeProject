3105f978f931610b706196da34f86f8444b0fb1e
Ahmed Karoui <karouii.ahmed@gmail.com>
2021-12-09 22:33:00 +0100

    working chat with multi room

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const dotenv = require('dotenv');
 const bodyparser = require('body-parser');
 const path = require('path');
 const connectDb = require('./server/database/databaseConnection');
+const socketio = require('socket.io');
+const http = require('http');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+
 //config
 dotenv.config({ path: 'config.env' })
 const PORT = process.env.SERVER_PORT || 8080;
@@ -27,11 +42,69 @@ app.use('/', require('./server/routes/router'));
 
 app.use('/users', require('./server/routes/usersRouter'));
 
+const botName = 'Bouhmid Bot';
 
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
+    socket.join(user.room);
 
-//start listening
-app.listen(PORT, (req, res) => {
-    console.log(`server started on  : http://localhost:${PORT}`);
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to our Chat!'));
 
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
 });
+
+app.get('/acceuilchat',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
+  app.get('/chat.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/chat.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
